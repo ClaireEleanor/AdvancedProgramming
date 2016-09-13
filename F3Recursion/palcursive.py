d7147297784cabbf72e2c2234179f4f7e19815fe
Nicholas Renner <nicholassrenner@gmail.com>
2016-09-13 16:37:19 -0400

    new

--- F3Recursion/palcursive.py
+++ F3Recursion/palcursive.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python
 
 def pal(n):
-  ls = str(n)
-  r = int(len(ls)/2)
-
-  for x in range(1,r+1):
-    if ls[x-1] != ls[len(ls)-x]:
-      return False
-  return True
+  if str(n) == str(n)[::-1]:
+    return True
+  else:
+   return True
 
 highpal=0
 
@@ -16,5 +13,5 @@ for i in range(100, 999):
     num = i * j
     if pal(num) == True and num > highpal:
       highpal = num
-print(highpal)
+      print(highpal)
 
