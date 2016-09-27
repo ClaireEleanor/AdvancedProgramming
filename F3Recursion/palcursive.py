6ca6f8678dfa993db6b8ffef506aafd8e895fc1c
Nicholas Renner <nicholassrenner@gmail.com>
2016-09-27 19:15:34 -0400

    fib

--- F3Recursion/palcursive.py
+++ F3Recursion/palcursive.py
@@ -3,9 +3,10 @@
 def ispalindrome(word):
     if len(word) < 2:
         return True
-    if word[0] != word[-1]:
+    if word[0] == word[-1]:
+        return ispalindrome(word[1:-1])
+    else:
         return False
-    return ispalindrome(word[1:-1])
 
 def ispalindrome1(word):
     return word == word[::-1]
