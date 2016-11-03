a213e3b84adab900597da9b1bfc211410a3d6cf8
Nicholas Renner <nicholassrenner@gmail.com>
2016-11-03 14:25:18 -0400

    answers

---
+++ challengeanswers/prob3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+file=open("roxanne.txt","r+")
+wordcount={}
+for word in file.read().split():
+    if word not in wordcount:
+        wordcount[word] = 1
+    else:
+        wordcount[word] += 1
+for k,v in wordcount.items():
+    print k, v
+
+print wordcount['Roxanne']
