3589848583ff405762909b7b9b7165c139e7ca83
rennergade <nicholassrenner@gmail.com>
2017-11-29 14:17:01 -0500

    shapes

--- 5-oop/shape.py
+++ 5-oop/shape.py
@@ -10,3 +10,25 @@ class Circle():
 
     def __str__(self):
         return "Circle has a radius of %.2f, an area of %.2f, and a perimeter of %.2f" % (self.x, self.area(), self.perimeter())
+
+class Rectangle():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def area(self):
+        return self.x*self.y
+
+    def perimeter(self):
+        return (2* self.x) + (2 * self.y)
+
+    def __str__(self):
+        return "Rectangle has a dimensions of x = %.2f and y = %.2f, an area of %.2f, and a perimeter of %.2f" % (self.x, self.y, self.area(), self.perimeter())
+
+class Square(Rectangle):
+    def __init__(self, x):
+        self.x = x
+        self.y = x
+
+    def __str__(self):
+        return "Square has a sidelength of %.2f, an area of %.2f, and a perimeter of %.2f" % (self.x, self.area(), self.perimeter())
