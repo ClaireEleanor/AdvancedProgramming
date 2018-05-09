7187a539850694ce816756de8529659c5ec38bf7
Nicholas Smith Renner <nrenner@seas.upenn.edu>
2018-05-09 13:44:31 -0400

    sieve

--- 10-techniques/sievenick.py
+++ 10-techniques/sievenick.py
@@ -7,6 +7,7 @@ def sieve(n):
         # If prime[p] is not changed, then it is a prime
         if (prime[p] == True):
         # Update all multiples of p
-            for i in range(p * 2, n+1, p):
+            for i in range(p ** 2, n, p):
                 prime[i] = False
         p += 1
+    return [2] + [p for p in range(3, n, 2) if prime[p]]
